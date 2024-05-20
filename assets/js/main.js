e26d1fce1f2155ba8f653a10a01b71d97e2be165
Mary Ogunmola <mary.ogunmola@ivantage.africa>
2024-05-21 00:04:32 +0100

    update

---
+++ assets/js/main.js
@@ -0,0 +1,351 @@
+/**
+* Template Name: Ninestars - v4.9.0
+* Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+  // leadership section modal
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const managers = document.querySelectorAll('.manager');
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modal-image');
+  const modalName = document.getElementById('modal-name');
+  const modalTitle = document.getElementById('modal-title');
+  const modalDetails = document.getElementById('modal-details');
+  const closeBtn = document.getElementById('close-btn');
+  const darkOverlay = document.createElement('div');
+  darkOverlay.className = 'dark-overlay hidden';
+  document.body.appendChild(darkOverlay);
+
+  managers.forEach(manager => {
+      manager.addEventListener('click', () => {
+          const name = manager.getAttribute('data-name');
+          const title = manager.getAttribute('data-title');
+          const details = manager.getAttribute('data-details');
+          const image = manager.getAttribute('data-image');
+
+          modalName.textContent = name;
+          modalTitle.textContent = title;
+          modalDetails.textContent = details;
+          modalImage.src = image;
+
+          modal.classList.add('show');
+          darkOverlay.classList.remove('hidden');
+          setTimeout(() => darkOverlay.classList.add('show'), 10); // Delay for transition effect
+      });
+  });
+
+  closeBtn.addEventListener('click', () => {
+      modal.classList.remove('show');
+      darkOverlay.classList.remove('show');
+      setTimeout(() => darkOverlay.classList.add('hidden'), 500); // Delay to match transition effect
+  });
+
+  darkOverlay.addEventListener('click', () => {
+      modal.classList.remove('show');
+      darkOverlay.classList.remove('show');
+      setTimeout(() => darkOverlay.classList.add('hidden'), 500); // Delay to match transition effect
+  });
+});
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let slides = document.getElementsByClassName("slides");
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   slides[slideIndex - 1].style.display = "block";  
+//   setTimeout(showSlides, 3000); // Change image every 3 seconds
+// }
+
+
+// function showSpecificSlide(n) {
+//   let slides = document.getElementsByClassName("slides");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slides[slideIndex - 1].style.display = "block";  
+// }
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll('.slides img');
+  slides.forEach(slide => slide.classList.remove('active'));
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].classList.add('active');
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+
+window.changeSlide = function(n) {
+  showSpecificSlide(slideIndex += n);
+};
+
+function showSpecificSlide(n) {
+  let slides = document.querySelectorAll('.slides img');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[slideIndex - 1].classList.add('active');
+}
+
+
+})()
+
---
+++ property.js
@@ -0,0 +1,113 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let data;
+
+  fetch('https://propertyapi.ivantage.africa/api/ivantage/properties?size=52&page=1')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((jsonData) => {
+      // Process the jsonData or assign it to data array
+      
+      data = jsonData;
+      console.log(data.propertyData)
+      console.log(data.data.propertyData)
+      renderData(data.data.propertyData, 
+        data.data.nextPage, 
+        data.data.previousPage, 
+        data.data.totalPages,
+        data.data.page);
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error.message);
+      alert('Unable to fetch property');
+    });
+
+
+    loadNextPage =(page) => {
+      fetch(`https://propertyapi.ivantage.africa/api/ivantage/properties?size=52&page=${page}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+        // Process the jsonData or assign it to data array
+        
+        data = jsonData;
+        console.log(data.propertyData)
+        console.log(data.data.propertyData)
+        renderData(data.data.propertyData, 
+          data.data.nextPage, 
+          data.data.previousPage, 
+          data.data.totalPages,
+          data.data.page);
+  
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error.message);
+        alert('Unable to fetch property');
+      });
+    
+    } 
+
+
+  function renderData(data, nextPage, previousPage, totalPage, page ) {
+    const row = document.querySelector('.row');
+    row.innerHTML = ''; 
+
+    for (let i = 0; i < data.length; i++) {
+      
+      const col = document.createElement('div');
+      const anchor = document.createElement('a');
+      anchor.href = `new.html?id=${data[i].id}`;
+      anchor.style.color = "black";
+      const coverDiv = document.createElement('div');
+      col.classList.add("col-lg-3", "p-4", "col-md-6", "col-sm-1");
+      coverDiv.classList.add("card", "border-0");
+      const cardImage = document.createElement('img');
+      cardImage.src = `${data[i].images[0]}`;
+      cardImage.classList.add("card-img-top");
+      const cardBody = document.createElement('div');
+      cardBody.innerHTML = `
+        <div>
+          <div class="top-right">${data[i].availableUnits} unit available</div>
+          <div class="card-body p-0">
+            <h5 class="card-title p-0" style="font-size: 17px;">${data[i].projectName}</h5>
+          </div>
+          <div class="felix">
+            <i style="color : red" class="fa-solid fa-location-dot"> &nbsp; </i>${data[i].address + ", " + data[i].state}
+          </div>
+          <div class="d-flex justify-content-between flex-row p-0 ps-0 m-0">
+            <div class="fw-bold fs-6 p-0">₦${data[i].price.toLocaleString()}</div>
+            <div>
+              <div class="felix">
+                <i class="fa-solid fa-bed p-0"></i> ${" " + data[i].numberOfBedrooms}
+              </div>
+            </div>
+          </div>
+        </div>`;
+      cardBody.classList.add("card-body");
+      coverDiv.appendChild(cardImage);
+      coverDiv.appendChild(cardBody);
+      anchor.appendChild(coverDiv);
+      col.appendChild(anchor);
+      row.appendChild(col);
+
+      const pagination  = document.querySelector('.pagination');
+      pagination.innerHTML = `
+      <button onclick="loadNextPage(${previousPage})" class="btn btn-success">Previous</button>
+      <span class="p-2 fw-bold">
+      ${page} of ${totalPage}
+      </span>
+     
+      <button onclick="loadNextPage(${nextPage})" class="btn btn-warning">Next</button>
+      `
+    }
+  }
+});
+
+
